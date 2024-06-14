3b4e9479f11989ad309387bd8e1c738b0a1ad495
Mohammad Khan <qcc2016@yahoo.com>
2024-06-14 16:32:48 -0400

    added functions

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,35 @@
-import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((response) => response.json())
+      .then((data) => setMovie(data));
+  }, [id]);
+
+  if (!movie) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        {movie.genres.map((genre, index) => (
+          <span key={index}>{genre}</span>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Movie;
